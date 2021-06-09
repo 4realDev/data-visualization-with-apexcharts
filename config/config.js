af3aaa5a9c0e96957eab2cb67f175559a6cd9079
Vladimir Brazhnik <v.brazhnik@web.de>
2021-06-09 17:47:05 +0200

    Add config.js file to use REST API in dev as well as in build mode

---
+++ config/config.js
@@ -0,0 +1,4 @@
+const dev = process.env.Node_ENV !== 'production' // check the node environment
+export const server = dev
+	? 'http://localhost:3000'
+	: 'https://ADJUST_ACCORDING_TO_DOMAIN_NAME.com'
