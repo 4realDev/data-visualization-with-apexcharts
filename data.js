3bb5342b7377daf46d02bd3541204a2b52e6eb02
Vladimir Brazhnik <v.brazhnik@web.de>
2021-06-09 17:38:14 +0200

    Transfer Mock data.js file from test-project

---
+++ data.js
@@ -0,0 +1,81 @@
+export const lineChartDataSeries = [
+	{
+		name: 'Music',
+		data: [1, 15, 26, 20, 33, 27],
+	},
+	{
+		name: 'Photos',
+		data: [3, 33, 21, 42, 19, 32],
+	},
+	{
+		name: 'Files',
+		data: [0, 39, 52, 11, 29, 43],
+	},
+]
+
+export const lineChartDataLabels = [
+	'New York',
+	'Los Angeles',
+	'Chicago',
+	'Houston',
+	'Philadelphia',
+	'Phoenix',
+	'San Antonio',
+	'San Diego',
+	'Dallas',
+	'San Jose',
+]
+
+export const barChartData = [
+	{
+		name: 'Music',
+		data: [
+			{ x: 'January', y: 14 },
+			{ x: 'February', y: 25 },
+			{ x: 'March', y: 21 },
+			{ x: 'April', y: 17 },
+			{ x: 'May', y: 12 },
+			{ x: 'June', y: 13 },
+			{ x: 'July', y: 11 },
+			{ x: 'August', y: 19 },
+			{ x: 'September', y: 23 },
+			{ x: 'October', y: 10 },
+			{ x: 'November', y: 14 },
+			{ x: 'December', y: 9 },
+		],
+	},
+	{
+		name: 'Photos',
+		data: [
+			{ x: 'January', y: 13 },
+			{ x: 'February', y: 23 },
+			{ x: 'March', y: 20 },
+			{ x: 'April', y: 8 },
+			{ x: 'May', y: 13 },
+			{ x: 'June', y: 27 },
+			{ x: 'July', y: 33 },
+			{ x: 'August', y: 12 },
+			{ x: 'September', y: 8 },
+			{ x: 'October', y: 11 },
+			{ x: 'November', y: 21 },
+			{ x: 'December', y: 7 },
+		],
+	},
+	{
+		name: 'Files',
+		data: [
+			{ x: 'January', y: 11 },
+			{ x: 'February', y: 17 },
+			{ x: 'March', y: 15 },
+			{ x: 'April', y: 15 },
+			{ x: 'May', y: 21 },
+			{ x: 'June', y: 14 },
+			{ x: 'July', y: 15 },
+			{ x: 'August', y: 13 },
+			{ x: 'September', y: 5 },
+			{ x: 'October', y: 15 },
+			{ x: 'November', y: 7 },
+			{ x: 'December', y: 11 },
+		],
+	},
+]
