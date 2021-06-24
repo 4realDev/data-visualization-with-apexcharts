c569cc97f44b607bd660c91d56aa2e85569b4b89
Vladimir Brazhnik <v.brazhnik@web.de>
2021-06-25 01:39:35 +0200

    Remove Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from 'react'
 
 import DataGraphsContainer from '../components/DataGraphsContainer'
